c042e71ba0279dfe566f658d31aa00368a21f0b0
GwadaPuk <gwadapuk@gmail.com>
2025-12-04 15:57:23 +0100

    fix logout

--- app_name.py
+++ app_name.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, session, redirect, url_for, jsonify
 from cas import CASClient
-from config import cas_server_url, cas_client_url, server_name
+from config import cas_server_url, cas_client_url
 
 app = Flask(__name__)
 app.secret_key = 'DFGtzenDRFz'
-app.config['SERVER_NAME'] = server_name
 
 cas_client = CASClient(
     version=3,
@@ -55,21 +54,15 @@ def login():
 
 @app.route('/logout')
 def logout():
-    redirect_url = url_for('logout_callback', _external=True)
+    redirect_url = cas_client_url + 'logout_callback'
     cas_logout_url = cas_client.get_logout_url(redirect_url)
-    app.logger.debug('CAS logout URL: %s', cas_logout_url)
 
     return redirect(cas_logout_url)
 
 @app.route('/logout_callback')
 def logout_callback():
-    # redirect from CAS logout request after CAS logout successfully
     session.pop('username', None)
     return 'Logged out from CAS. <a href="/login">Login</a>'
-
-def start():
-    app.run()
-
 
 if __name__ == '__main__':
     app.run()
