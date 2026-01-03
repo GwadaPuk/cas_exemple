b84fa0b0ef617226f87c56044255905d659c9e5e
GwadaPuk <gwadapuk@gmail.com>
2026-01-03 16:29:34 +0100

    modifications non-authorized access redirect

--- app_name.py
+++ app_name.py
@@ -94,7 +94,7 @@ def validate_session():
         res = make_response(render_template_string(
             """
             <h1>Accès non-autorisé, veuillez vous <a href="https://lotte.bouillabaisse.ec-m.fr/login?redirect=https://vive.bouillabaisse.ec-m.fr/login">connecter</a>;
-            """), 403)
+            """), 401)
         return res
 
 if __name__ == '__main__':
